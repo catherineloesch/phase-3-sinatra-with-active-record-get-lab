a6e1fc975a78f2ea37c023fa58788afa74d08745
Catherine Loesch <catherine.loesch@protonmail.com>
2022-03-25 00:43:52 +0000

    lab completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
-  # add routes
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: { baked_goods: { only: [:name, :price] }})
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json(only: [:name, :price])
+  end
+  
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.all.order(price: :desc).first
+    most_expensive.to_json(only: [:name, :price])
+  end
 
 end
